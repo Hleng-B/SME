56f428e9d9b8ae72c98c1cf66cc34cb43011fdf8
Anesuzues <anesukamombe8@gmail.com>
2026-01-12 14:19:17 +0200

    Update Google Apps Script URL to new deployment

---
+++ fix-voucher-link.js
@@ -0,0 +1,13 @@
+// REPLACE THIS LINE IN YOUR code.gs (around line 33):
+// template.voucherLink = "https://nobztech.co.za/voucher?code=" + voucherCode;
+
+// WITH ONE OF THESE OPTIONS:
+
+// Option 1: Point back to your main landing page
+template.voucherLink = "https://nobztech.co.za/";
+
+// Option 2: Point to the voucher page we created (if you upload it)
+template.voucherLink = "https://nobztech.co.za/voucher.html";
+
+// Option 3: Remove the link entirely and just show the voucher code
+// (Delete the voucherLink line and update your email template)
